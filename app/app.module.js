bd6743c121accca744f1a114d0d1f06408c90a43
François Wagner <francois.wagner@yahoo.fr>
2016-11-17 16:29:33 +0100

    Add angular-cookies to save the username even if a refresh happended

--- app/app.module.js
+++ app/app.module.js
@@ -4,11 +4,22 @@ angular.module('guessTheWordApp', [
   'core',
   'registerForm',
   'game',
-  'highscores'
-]).controller('ApplicationController', function ($scope) {
+  'highscores',
+  'ngCookies'
+]).controller('ApplicationController', ['$scope', '$cookies', function ($scope, $cookies) {
 	$scope.currentUser = null;
 	
 	$scope.setCurrentUser = function (user) {
 		$scope.currentUser = user;
+		
+		if(!$cookies.get('user')){
+			$cookies.put('user', $scope.currentUser);
+		}
 	}
-});
+	
+	// Retrieving username if exists
+	if($cookies.get('user')){
+		$scope.setCurrentUser($cookies.get('user'));
+	}
+	  
+}]);
